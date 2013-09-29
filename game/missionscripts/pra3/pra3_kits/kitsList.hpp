afc8cd9a49bc69fef9e2bdc6e79ce5eb1098576e
james2464 <james2464@99a92db3-b023-4748-beca-ae334d4d7cd8>
2013-09-29 02:37:58 +0000

    Added AAF faction kitlist.

--- game/missionscripts/pra3/pra3_kits/kitsList.hpp
+++ game/missionscripts/pra3/pra3_kits/kitsList.hpp
@@ -29,6 +29,12 @@ class PRA3_teams
 		name = "Iran";
 		flag = "\A3\Data_F\Flags\Flag_CSAT_CO.paa";
 	};
+	class AAF
+	{
+		side = SIDE_RESISTANCE;
+		name = "AAF";
+		flag = "\A3\Data_F\Flags\Flag_AAF_CO.paa";
+	};
 };
 
 class PRA3_kits
@@ -554,6 +560,281 @@ class PRA3_kits
 			};
 		};
 		class Marksman_IN : Rifleman_IN
+		{
+			name = "Marksman";
+			picture = "pra3\pra3_ui\kits\kit_img\sniper_88.paa";
+			icon = "pra3\pra3_ui\kits\kit_img\sniper_small_88.paa";
+
+			class Requirements
+			{
+				type = LIMIT_SQUAD;
+
+				perEvery    = 6;
+				minMembers  = 6;
+				maxPerSquad = 1;
+			};
+			availableAt[] = {SPAWN_MAIN};
+
+			class Clothing : Clothing
+			{
+				vest = "V_ChestrigF_oli";
+				headgear = "H_HelmetIA_camo";
+			};
+
+			class Variants : Variants
+			{
+				class Variant1 : Variant1
+				{
+					class Primary : Primary
+					{
+						weapon      = "srifle_EBR_F";
+						optics      = "optic_Arco";
+						magazines[] = {{"20Rnd_762x51_Mag", 8}};
+					};
+					class Secondary
+					{
+					};
+					class Backpack
+					{
+					};
+
+					explosives[] = {};
+				};
+			};
+		};
+		
+		class Rifleman_IND : Rifleman_IN
+		{
+			scope = public;
+			team = AAF;
+
+			class Clothing
+			{
+				uniform  = "U_I_CombatUniform";
+				headgear = "H_HelmetIA_net";
+				goggles  = "";
+				vest     = "V_PlateCarrierIA2_dgtl";
+			};
+
+			class Variants
+			{
+				class Variant1
+				{
+					name = "Standard";
+					class Pistol
+					{
+					};
+					class Primary
+					{
+						weapon      = "arifle_Katiba_F";
+						optics      = "optic_ACO_grn";
+						rail        = "";
+						muzzle      = "";
+						magazines[] = {{"30Rnd_65x39_caseless_green_mag_Tracer", 9}};
+					};
+					class Secondary
+					{
+					};
+					class Backpack
+					{
+						backpack    = "B_Carryall_oli";
+						weapons[]   = {};
+						magazines[] = {{"30Rnd_65x39_caseless_green_mag_Tracer", 12}};
+					};
+
+					explosives[] = {{"HandGrenade", 2}, {"SmokeShell", 2}};
+					items[] = {{"FirstAidKit", 1}};
+				};
+				class Variant2 : Variant1
+				{
+					name = "AR Support";
+
+					class Backpack : Backpack
+					{
+						backpack    = "B_TacticalPack_oli";
+						magazines[] = {{"200Rnd_65x39_belt_Tracer_Green", 5}};
+					};
+				};
+				class Variant3 : Variant1
+				{
+					name = "Grenadier Support";
+
+					class Backpack : Backpack
+					{
+						backpack    = "B_Tacticalpack_oli";
+						magazines[] = {{"1Rnd_HE_Grenade_shell", 20}, {"1Rnd_Smoke_Grenade_shell", 10}};
+					};
+				};
+			};
+		};
+		class SquadLeader_IND : Rifleman_IND
+		{
+			name = "Squad Leader";
+			picture = "pra3\pra3_ui\kits\kit_img\sqleader_88.paa";
+			icon = "pra3\pra3_ui\kits\kit_img\sqleader_small_88.paa";
+
+			class Requirements
+			{
+				type = LIMIT_SL;
+			};
+
+			class Clothing : Clothing
+			{
+				uniform = "U_I_CombatUniform_tshirt";
+				headgear = "H_HelmetIA";
+				vest     = "V_PlateCarrierIA1_dgtl";
+			};
+
+			class Variants : Variants
+			{
+				class Variant1 : Variant1
+				{
+					name = "Standard";
+					class Pistol
+					{
+						weapon      = "hgun_Rook40_F";
+						magazines[] = {{"16Rnd_9x21_Mag", 3}};
+					};
+					class Primary : Primary
+					{
+						optics      = "optic_Arco";
+					};
+					class Secondary
+					{
+					};
+					class Backpack
+					{
+					};
+
+					explosives[] = {{"HandGrenade", 2}, {"SmokeShell", 4}, {"SmokeShellRed", 2}};
+					items[] = {{"FirstAidKit", 3}, {"Rangefinder", 1}};
+				};
+			};
+		};
+		class Medic_IND : Rifleman_IND
+		{
+			name = "Combat Medic";
+			picture = "pra3\pra3_ui\kits\kit_img\medic_88.paa";
+			icon = "pra3\pra3_ui\kits\kit_img\medic_small_88.paa";
+
+			class Requirements
+			{
+				type = LIMIT_SQUAD;
+
+				perEvery    = 3;
+				minMembers  = 3;
+				maxPerSquad = 1;
+			};
+
+			class Clothing : Clothing
+			{
+				uniform = "U_I_CombatUniform_shortsleeve";
+				vest = "V_BandollierB_oli";
+				headgear = "H_HelmetIA_camo";
+			};
+
+			class Variants : Variants
+			{
+				class Variant1 : Variant1
+				{
+					class Primary : Primary
+					{
+						weapon      = "arifle_Katiba_C_F";
+					};
+					class Secondary
+					{
+					};
+					class Backpack
+					{
+						backpack = "B_AssaultPack_khk";
+						weapons[] = {{"Medikit", 1}, {"FirstAidKit", 10}};
+					};
+
+					explosives[] = {{"HandGrenade", 2}, {"SmokeShell", 5}};
+					items[] = {};
+				};
+			};
+		};
+
+		class Machinegunner_IND : Rifleman_IND
+		{
+			name = "Automatic Rifleman";
+			picture = "pra3\pra3_ui\kits\kit_img\autorifleman_88.paa";
+			icon = "pra3\pra3_ui\kits\kit_img\autorifleman_small_88.paa";
+
+			class Requirements
+			{
+				type = LIMIT_SQUAD;
+
+				perEvery    = 5;
+				minMembers  = 5;
+				maxPerSquad = 1;
+			};
+
+			class Clothing : Clothing
+			{
+				uniform = "U_I_CombatUniform_shortsleeve";
+			};
+
+			class Variants : Variants
+			{
+				class Variant1 : Variant1
+				{
+					class Primary : Primary
+					{
+						weapon      = "LMG_Mk200_F";
+						magazines[] = {{"200Rnd_65x39_cased_Box_Tracer", 3}};
+					};
+					class Secondary
+					{
+					};
+					class Backpack
+					{
+					};
+				};
+			};
+		};
+		class Grenadier_IND : Rifleman_IND
+		{
+			name = "Grenadier";
+			picture = "pra3\pra3_ui\kits\kit_img\grenadier_88.paa";
+			icon = "pra3\pra3_ui\kits\kit_img\grenadier_small_88.paa";
+
+			class Requirements
+			{
+				type = LIMIT_SQUAD;
+
+				perEvery    = 5;
+				minMembers  = 5;
+				maxPerSquad = 1;
+			};
+
+			class Clothing : Clothing
+			{
+				vest = "V_PlateCarrierIAGL_dgtl";
+			};
+
+			class Variants : Variants
+			{
+				class Variant1 : Variant1
+				{
+					class Primary : Primary
+					{
+						weapon      = "arifle_Katiba_GL_F";
+						magazines[] = {{"30Rnd_65x39_caseless_green_mag_Tracer", 9}, {"1Rnd_HE_Grenade_shell", 8}, {"1Rnd_Smoke_Grenade_shell", 4}};
+					};
+					class Secondary
+					{
+					};
+					class Backpack
+					{
+					};
+
+					explosives[] = {};
+				};
+			};
+		};
+		class Marksman_IND : Rifleman_IND
 		{
 			name = "Marksman";
 			picture = "pra3\pra3_ui\kits\kit_img\sniper_88.paa";
