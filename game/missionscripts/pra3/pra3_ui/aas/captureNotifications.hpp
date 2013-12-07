fab5357727ac748b38013c5cb9dc30b9e3550ec7
shay_gman <shay_gman@99a92db3-b023-4748-beca-ae334d4d7cd8>
2013-12-07 15:27:24 +0000

    Capture notifications should be blue if they are good and red if they are bad ;)

--- game/missionscripts/pra3/pra3_ui/aas/captureNotifications.hpp
+++ game/missionscripts/pra3/pra3_ui/aas/captureNotifications.hpp
@@ -1,6 +1,6 @@
-class PRA3_AAS_captureNotification
+class PRA3_AAS_captureNotificationFriendly
 {
-	colorIconPicture[] = {"%5"};
+	colorIconPicture[] = {0,0,0.5,1};
 	description = "Objective %1 captured by %2";
 	iconPicture = "%3";
 	iconText = "%4";
@@ -9,10 +9,32 @@ class PRA3_AAS_captureNotification
 	title = "Objective Captured";
 };
 
-class PRA3_AAS_neutralizedNotification
+class PRA3_AAS_neutralizedNotificationFriendly
 {
-	colorIconPicture[] = {"%5"};
-	description = "%1 lost control of Objective %2";
+	colorIconPicture[] = {0,0,0.5,1};
+	description = "Objective %1 neutralized by %2";
+	iconPicture = "%3";
+	iconText = "%4";
+	priority = 4;
+	sound = "sectorCaptured";
+	title = "Objective Neutralized";
+};
+
+class PRA3_AAS_captureNotificationHostile
+{
+	colorIconPicture[] = {0.5,0,0,1};
+	description = "Objective %1 captured by %2";
+	iconPicture = "%3";
+	iconText = "%4";
+	priority = 4;
+	sound = "sectorCaptured";
+	title = "Objective Captured";
+};
+
+class PRA3_AAS_neutralizedNotificationHostile
+{
+	colorIconPicture[] = {0.5,0,0,1};
+	description = "Objective %1 neutralized by %2";
 	iconPicture = "%3";
 	iconText = "%4";
 	priority = 4;
