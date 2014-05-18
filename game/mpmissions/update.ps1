4fc9ef99c7bef4f73a5ca8fda92d000b0483b1c5
deadfast <Deadfast@users.noreply.github.com>
2014-05-18 10:37:38 +0000

    Improved update script to better handle presence of unversioned mission.

--- game/mpmissions/update.ps1
+++ game/mpmissions/update.ps1
@@ -1,11 +1,21 @@
 # Update the working directory and report the revision each mission and its external pra3 directory are at.
 
+echo "Updating working copy...";
+
 svn update
 
+echo "Fetching and writing SVN revisions for each mission...";
+echo "";
+
 Get-ChildItem | Where {$_.PSIsContainer} | foreach {
-	$missionRev = svn info --xml $_.FullName;
+	echo ($_.Name);
+
+	# First make sure the mission dir is versioned
+	svn info $_.FullName > $null 2>&1;
+
 	if ($?)
 	{
+		$missionRev = svn info --xml $_.FullName;
 		$missionRev = ([xml]$missionRev).info.entry.commit.revision;
 
 		$coreRev = svn info --xml ($_.FullName + "\pra3");
@@ -13,10 +23,19 @@ Get-ChildItem | Where {$_.PSIsContainer} | foreach {
 		{
 			$coreRev = ([xml]$coreRev).info.entry.commit.revision;
 
-			echo ($_.Name + " Mission revision: " + $missionRev + " Core revision: " + $coreRev);
-			
+			echo (" > Mission revision: " + $missionRev);
+			echo (" > Core revision: " + $coreRev);
+
 			echo $missionRev > ($_.FullName + "\revision.txt");
 			echo $coreRev > ($_.FullName + "\pra3\revision.txt");
 		}
 	}
+	else
+	{
+		echo " > Not versioned, skipping";
+	}
+
+	echo "";
 }
+
+echo "DONE!";
